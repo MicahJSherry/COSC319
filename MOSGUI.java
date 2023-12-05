e81098ddec35b4a60516dea2ca094501caed1feb
Inquisitorious <133789797+Inquisitorious@users.noreply.github.com>
2023-12-05 11:54:22 -0500

    Updated MOSGUI.java
    Uses Staff class now

--- MOSGUI.java
+++ MOSGUI.java
@@ -3,16 +3,15 @@ import java.util.ArrayList;
 public class MOSGUI {
 	public static void main(String[] args) {
 
-		ArrayList<Doctor> doctors = new ArrayList<Doctor>();
-		ArrayList<Nurse> nurses = new ArrayList<Nurse>();
+		ArrayList<Staff> Staffs = new ArrayList<Staff>();
 		
-		Doctor doc1 = new Doctor("211", "Thomas", "Hobbes");
-		Doctor doc2 = new Doctor("245", "Adam", "Smith");
-		Doctor doc3 = new Doctor("289", "Francis", "Bacon");
+		Staff Staff1 = new Staff("211", "Thomas", "Hobbes");
+		Staff Staff2 = new Staff("245", "Adam", "Smith");
+		Staff Staff3 = new Staff("289", "Francis", "Bacon");
 		
-		Nurse nurse1 = new Nurse("120", "David", "Hume");
-		Nurse nurse2 = new Nurse("104", "John", "Locke");
-		Nurse nurse3 = new Nurse("167", "George", "Berkeley");
+		Staff Staff4 = new Staff("120", "David", "Hume");
+		Staff Staff5 = new Staff("104", "John", "Locke");
+		Staff Staff6 = new Staff("167", "George", "Berkeley");
 		
 	}
 	
