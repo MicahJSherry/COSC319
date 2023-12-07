64f62dd04bf44b6e1cd218b780560e5ce269ec61
Micah Sherry <MicahJSherry@gmail.com>
2023-12-07 12:12:13 -0500

    Added changeStatus and new Shedual constructor

--- Schedule.java
+++ Schedule.java
@@ -6,11 +6,12 @@ public class Schedule {
 	private String dosage;
 	private String drug_id;
 	private String pickup_date;
-	private String status;
+	private String status = "Not Admisistered";
 	
 	public Schedule(String patient_id, String drug_id, String dosage, String doc_last, String nurse_last,
-			String pickup_date, String status) {
-		super();
+					String pickup_date, String status) {
+		//use the other one
+				super();
 		this.patient_id = patient_id;
 		this.doc_last_name = doc_last;
 		this.nurse_last_name = nurse_last;
@@ -18,6 +19,19 @@ public class Schedule {
 		this.drug_id = drug_id;
 		this.pickup_date = pickup_date;
 		this.status = status;
+	}
+	public Schedule(String patient_id, String drug_id, String dosage, String doc_last, String pickup_date) {
+		super();
+		this.patient_id = patient_id;
+		this.doc_last_name = doc_last;
+		this.dosage = dosage;
+		this.drug_id = drug_id;
+		this.pickup_date = pickup_date;
+	}
+	public void changeStatus(String newStatus,String nurse_last){
+		this.status = newStatus;
+		this.nurse_last_name = nurse_last;
+
 	}
 
 	public String getPatient_id() {
