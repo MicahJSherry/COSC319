72b17dd06d835cf9482a4c4667bd70fd7f7601af
Inquisitorious <133789797+Inquisitorious@users.noreply.github.com>
2023-12-07 11:23:16 -0500

    Updated to fit with current version of MOSGUI

--- Schedule.java
+++ Schedule.java
@@ -1,20 +1,19 @@
-import java.util.ArrayList;
 
 public class Schedule {
 	private String patient_id;
-	private String doc_id;
-	private String nurse_id;
+	private String doc_last_name;
+	private String nurse_last_name;
 	private String dosage;
 	private String drug_id;
 	private String pickup_date;
-	private Boolean status;
+	private String status;
 	
-	public Schedule(String patient_id, String doc_id, String nurse_id, String dosage, String drug_id,
-			String pickup_date, Boolean status) {
+	public Schedule(String patient_id, String drug_id, String dosage, String doc_last, String nurse_last,
+			String pickup_date, String status) {
 		super();
 		this.patient_id = patient_id;
-		this.doc_id = doc_id;
-		this.nurse_id = nurse_id;
+		this.doc_last_name = doc_last;
+		this.nurse_last_name = nurse_last;
 		this.dosage = dosage;
 		this.drug_id = drug_id;
 		this.pickup_date = pickup_date;
@@ -25,12 +24,12 @@ public class Schedule {
 		return patient_id;
 	}
 
-	public String getDoc_id() {
-		return doc_id;
+	public String getDoc_last() {
+		return doc_last_name;
 	}
 
-	public String getNurse_id() {
-		return nurse_id;
+	public String getNurse_last() {
+		return nurse_last_name;
 	}
 
 	public String getDosage() {
@@ -45,27 +44,19 @@ public class Schedule {
 		return pickup_date;
 	}
 
-	public Boolean getStatus() {
+	public String getStatus() {
 		return status;
 	}
 	
-	public String printSchedule(ArrayList<Staff> doctors, ArrayList<Nurse> nurses) {
-		Staff prescriber = new Staff(doc_id);
-		int doc_index = doctors.indexOf(prescriber);
-		
-		Nurse administer = new Nurse(nurse_id);
-		int nurse_index = nurses.indexOf(administer);
-		
-		String doc_last_name = doctors.get(doc_index).getLast_name();
-		String nurse_name = nurses.get(nurse_index).getFirst_name() + " " + nurses.get(nurse_index).getLast_name();
+	public String printSchedule() {
 		
 		String schedule = "\nPatient ID: " + patient_id +
-				"\nDrug ID: " + drug_id + 
-				"\nDosage: " + dosage + 
-				"\nPrescribed by Dr. " + doc_last_name + 
-				"\nAdministered by " + nurse_name + 
-				"\nPickup date: " + pickup_date +
-				"\nPicked up: " + status + "\n";
+				"\n\nDrug ID: " + drug_id + 
+				"\n\nDosage: " + dosage + 
+				"\n\nPrescribed by Dr. " + doc_last_name + 
+				"\n\nAdministered by " + nurse_last_name + 
+				"\n\nPickup date: " + pickup_date +
+				"\n\nPicked up: " + status + "\n";
 		return schedule;
 	}
 }
