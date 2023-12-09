f9bca1050beca0c39298baa3a0582de818c6c3aa
mckenzielc <152522529+mckenzielc@users.noreply.github.com>
2023-12-08 23:56:27 -0500

    Update patient.java
    removed status from constructor (in main branch already)

--- patient.java
+++ patient.java
@@ -1,5 +1,6 @@
 
-public class patient extends Schedule{
+public class patient extends Schedule
+{
 
 	private String patient_id;
 	private String doc_id;
@@ -10,8 +11,9 @@ public class patient extends Schedule{
 	private String status;
 
 	public patient(String patient_id, String doc_id, String nurse_id, String dosage, String drug_id, String pickup_date,
-			String status) {
-		super(patient_id, doc_id, nurse_id, dosage, drug_id, pickup_date, status);
+			String status) 
+	{
+		super(patient_id, doc_id, nurse_id, dosage, drug_id, pickup_date);
 		this.patient_id = patient_id;
 		this.doc_id = doc_id;
 		this.nurse_id = nurse_id;
@@ -21,11 +23,13 @@ public class patient extends Schedule{
 		this.status = status;
 	}
 	
-	public String setDrug_id(String drug_id) {
+	public String setDrug_id(String drug_id) 
+	{
 		this.drug_id = drug_id;
 		String setDrug_Id = null;
 		return setDrug_Id;
 	}
+
 
 	
 
